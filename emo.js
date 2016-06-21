0b4082621211739ad19d96f46b7e5fbe4ed390d7
rival38 <rival.3811@gmail.com>
2016-06-21 13:49:30 +0700

    Update emo.js

--- emo.js
+++ emo.js
@@ -1,22 +1,6 @@
 <script type='text/javascript'>
 //<![CDATA[
 // Created by Blog X Komo
-document.getElementsByClassName = function(clsName){
-    var retVal = new Array();
-    var elements = document.getElementsByTagName("*");
-    for(var i = 0;i < elements.length;i++){
-        if(elements[i].className.indexOf(" ") >= 0){
-            var classes = elements[i].className.split(" ");
-            for(var j = 0;j < classes.length;j++){
-                if(classes[j] == clsName)
-                    retVal.push(elements[i]);
-            }
-        }
-        else if(elements[i].className == clsName)
-            retVal.push(elements[i]);
-    }
-    return retVal;
-}
 
 function addSmiley(){
   var postBodyClass = (postBodyClass) ? postBodyClass : 'post-body';
