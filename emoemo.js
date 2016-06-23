f32ceff4c2d0cedbe14c3b1d2b6b366af143ac3e
rival38 <rival.3811@gmail.com>
2016-06-23 19:57:47 +0700

    Add files via upload

---
+++ emoemo.js
@@ -0,0 +1,62 @@
+//<![CDATA[
+function addSmiley(){
+  var postBodyClass = (postBodyClass) ? postBodyClass : 'post-body';
+  var d = document.getElementsByClassName(postBodyClass);
+  for(var i=0;i<d.length;i++){
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*sedih/g, ' <img src="https://4.bp.blogspot.com/-W5J9R40AAcM/V2tgPjpau5I/AAAAAAAACSA/vMcbIYv38fAB6ihlU519LT0krvW6xGitQCLcB/s1600/sedih.gif" alt="emoticon lagi sedih" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*stres/g, ' <img src="https://3.bp.blogspot.com/-3pmlrbPj0zw/V2tgbW2YF0I/AAAAAAAACSI/JkglGg9eDkE5GcEkGN5919E4O17aTZPfACLcB/s1600/stres.gif" alt="emoticon lagi stres" style="border:0; margin:0; padding:0;" id="new" /> ');
+       	d[i].innerHTML = d[i].innerHTML.replace(/\s:-*mewek/g, ' <img src="https://4.bp.blogspot.com/-BvYb5_nfryM/V2tg2l9NG2I/AAAAAAAACSU/3doTGW72csUyzNAVW8FTwf87eIlxh7NdgCLcB/s1600/mewek.gif" alt="emoticon mau mewek" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*hai/g, ' <img src="https://4.bp.blogspot.com/-BQz_8hVRJi4/V2thFnOT4EI/AAAAAAAACSg/J2FrlJOC1FUGSNaoyaB1zLYES0Hp6NE3gCLcB/s1600/hai.gif" alt="emoticon hai" style="border:0; margin:0; padding:0;" id="new" /> ');
+		d[i].innerHTML = d[i].innerHTML.replace(/\s:-*hua/g, ' <img src="https://3.bp.blogspot.com/-eweqWW6cEa4/V2tlwk7wmMI/AAAAAAAACUA/ZRoYjA-uk1QUvzUwtx5Spt3CiWpES-_fQCLcB/s1600/hua.gif" alt="emoticon mau hua" style="border:0; margin:0; padding:0;" id="new" /> ');
+		d[i].innerHTML = d[i].innerHTML.replace(/\s:-*haha/g, ' <img src="https://1.bp.blogspot.com/-9drpDsYTrpQ/V2tlbov-oJI/AAAAAAAACTc/qmlYY53eyWg4sNXzr9QqX-HSRlD1X7YwQCLcB/s1600/haha.gif" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*hehe/g, ' <img src="https://2.bp.blogspot.com/-dtET7UqwAvc/V2tlh9sHxTI/AAAAAAAACTk/zfyXh4-_WnkS3sZZACmlrMR6FL-yVpEHQCLcB/s1600/hehe.gif" style="border:0; margin:0; padding:0;" id="new" /> ');
+       	d[i].innerHTML = d[i].innerHTML.replace(/\s:-*hihi/g, ' <img src="https://4.bp.blogspot.com/-NqJjH37qeC0/V2tlr6YIwpI/AAAAAAAACT4/YeK-omczZ74KbuKX6_JY0H-IuDDVwL_TQCLcB/s1600/hihi.gif" alt="emoticon hihihi" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*wkwk/g, ' <img src="https://4.bp.blogspot.com/-2zoYAoo3nhQ/V2vXFTJ_5-I/AAAAAAAACXo/QJvknzZI7807nNSvufI1k0HUuYP63im0wCLcB/s1600/wkwk.gif" alt="emoticon kwkwkw" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*ckck/g, ' <img src="https://2.bp.blogspot.com/-_ZXwY8LGs8s/V2tlNrbMo6I/AAAAAAAACTE/wrL-TJPRdI0SZyazDPhLefQ57HkPEU0-gCLcB/s1600/ckck.gif" alt="emoticon kckckck" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*ngakak/g, ' <img src="https://2.bp.blogspot.com/-Lu856kjGUSw/V2vVqvdEc-I/AAAAAAAACVE/Ghtnlenim9QKZWEewLiLsrddG357RIFHQCLcB/s1600/ngakak.gif" alt="emoticon ngakak" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*ketawa/g, ' <img src="https://1.bp.blogspot.com/-576vlFjfXDg/V2tl-mDLl4I/AAAAAAAACUc/-RHMnxd5BM4S_5T1ZWGrxpR9qa7sLIYKQCLcB/s1600/ketawa.gif" alt="emoticon ketawa" style="border:0; margin:0; padding:0;" id="new" /> ');
+		d[i].innerHTML = d[i].innerHTML.replace(/\s:-*info/g, ' <img src="https://4.bp.blogspot.com/-sHiGT9Cxac8/V2tl07G4AjI/AAAAAAAACUI/8h8gbOEXyfwof0-1gRzZoEGultGIYnpmACLcB/s1600/info.gif" alt="emoticon info" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*oh/g, ' <img src="https://4.bp.blogspot.com/-TyjGwUQZKzQ/V2vV6wAKIFI/AAAAAAAACVc/v_NCyPq1M2oSOBWdkDnSCqwjsEpnuGNygCLcB/s1600/oh.gif" alt="emoticon oeye" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*no/g, ' <img src="https://3.bp.blogspot.com/-x1ssPAr6hVo/V2vV2MnvhrI/AAAAAAAACVU/cbXJ86BnPCgSXxQwmS89DSHP3Rj-R6e3QCLcB/s1600/no.gif" alt="emoticon no" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*yes/g, ' <img src="https://1.bp.blogspot.com/-r9WrfxhwcOg/V2vXKoItAFI/AAAAAAAACXw/qd4GagLcps8gxozdw1YgQp5YtzEjcrlAACLcB/s1600/yes.gif" alt="emoticon yes" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*ok/g, ' <img src="https://4.bp.blogspot.com/-6v3c5qfZhs4/V2vWAEUh4FI/AAAAAAAACVo/A8Jc2sm7BccN2UFVzC1IH693FRY5KiAOQCLcB/s1600/ok.gif" alt="emoticon ok" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*bye/g, ' <img src="https://3.bp.blogspot.com/-0sZwndLOn3Q/V2tlIlQglfI/AAAAAAAACS8/2Z5KbK_3Vxk59Detrng_laV3GGZQ24bSQCLcB/s1600/bye.gif" alt="emoticon uh" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*thanks/g, ' <img src="https://4.bp.blogspot.com/-QDFjZZaZ5OI/V2vWuckYLQI/AAAAAAAACW4/N8IkDpfEFhUty1h1bTJXdpXR1jla_fgSACLcB/s1600/thanks.gif" alt="emoticon thanks" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*cool/g, ' <img src="https://1.bp.blogspot.com/-Rmj-zlPjRvA/V2tlSqlf9qI/AAAAAAAACTM/uVTsPQkgfhMK52VUJRmMwhuDh8iZ7DfVQCLcB/s1600/cool.gif" alt="emoticon cool" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*wah/g, ' <img src="https://2.bp.blogspot.com/-lV9o8pAR4OQ/V2vW3qpkBPI/AAAAAAAACXM/GERN1Bpj8B4YDPIMJpNCRj0aVAPhaJdewCLcB/s1600/wah.gif" alt="emoticon wah" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*ups/g, ' <img src="https://4.bp.blogspot.com/-xiVh4E4MRzU/V2vWy3SbbkI/AAAAAAAACXE/AtBCIccqGFcDTQRp6tKYFajdzxqOBLcjgCLcB/s1600/ups.gif" alt="emoticon ups" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*hi/g, ' <img src="https://2.bp.blogspot.com/-V65iO-ceEVA/V2tlnWihtcI/AAAAAAAACTs/E9294_J0gBcYeofoPkzDw8YN2XxR8t-DgCLcB/s1600/hi.gif" alt="emoticon comment" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*juang/g, ' <img src="https://1.bp.blogspot.com/-ZqRlOKTWC_A/V2tl5vvXMgI/AAAAAAAACUU/8NPZGIqhT9cfukQKfEvSEkLLUCsYl_iBQCLcB/s1600/juang.gif" alt="emoticon dollar" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*senang/g, ' <img src="https://2.bp.blogspot.com/-WtbWprxRbPg/V2vWOEwjILI/AAAAAAAACWA/nYSF7IIZmJ4XuGSvVgx4VKXYxrOHpuJ-wCLcB/s1600/senang.gif" alt="emoticon E-mail" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*benci/g, ' <img src="https://1.bp.blogspot.com/-GeXqJY432dE/V2tlDgr0KrI/AAAAAAAACS0/h1AlDH0b3kMZY6qXM3DeW37ni-VeyWQkQCLcB/s1600/benci.gif" alt="emoticon photo" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*tembak/g, ' <img src="https://1.bp.blogspot.com/-bpKxf25vF9g/V2vWpjM4wxI/AAAAAAAACWs/tZ_S_Uj1l646Q1KsKIHGZNg40UX5TpScwCLcB/s1600/tembak.gif" alt="emoticon video" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*gasak/g, ' <img src="https://1.bp.blogspot.com/-lExrsJKn-hs/V2tlXFj6blI/AAAAAAAACTU/XfxO4dg66xMAugxW6GZQkE9Wy8RaVYHUgCLcB/s1600/gasak.gif" alt="emoticon lock" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*love/g, ' <img src="https://2.bp.blogspot.com/-fCMPtqAu5Eo/V2tmTMT8iiI/AAAAAAAACUo/2Oq5OuAFO4EKqI7JUUYlqzYFi9CU_ED4gCLcB/s1600/love.gif" alt="emoticon love" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*warning/g, ' <img src="https://4.bp.blogspot.com/-7kDcFfX5fRU/V2vW8E_-u5I/AAAAAAAACXU/FOcFXcbEEcI2vuFnj5JZEnLx-OUkF-cOQCLcB/s1600/warning.gif" alt="emoticon warning" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*mati/g, ' <img src="https://1.bp.blogspot.com/-EeH6Fn2ITm0/V2vVbTarKOI/AAAAAAAACU8/tPgXn4DepXw-JPls-XpU1OFy5PuzGbkbgCKgB/s1600/mati.gif" alt="emoticon salah" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*ngambul/g, ' <img src="https://4.bp.blogspot.com/-H--g1_5Qps8/V2vVv2XoYDI/AAAAAAAACVM/ekGIfOwZR0I7d7oODJ9zU60JUjXtEvSlwCLcB/s1600/ngambul.gif" alt="emoticon E-mail" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*sinau/g, ' <img src="https://2.bp.blogspot.com/-coPDv34QLiY/V2vWZN0lMeI/AAAAAAAACWM/_ajX3-ZLazMi2EQs2NBNJSg-UKBApRRTQCLcB/s1600/sinau.gif" alt="emoticon Copyright" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*tanya/g, ' <img src="https://4.bp.blogspot.com/-OATPZSVu7Vg/V2vWlTTuhXI/AAAAAAAACWk/rr1wvEbCzbIfsYXSV_A6FBA_VFhXRX15QCLcB/s1600/tanya.gif" alt="emoticon search" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*stres1/g, ' <img src="https://1.bp.blogspot.com/-3nk41Z3h6aU/V2vWf8TbpXI/AAAAAAAACWY/4_rVSWWgOeU_r7TzJN1zYXMG-y9Y3fAowCLcB/s1600/stres1.gif" alt="emoticon peri" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*rokok/g, ' <img src="https://4.bp.blogspot.com/-H6g4ZMtx_Vo/V2vWEcCr9tI/AAAAAAAACVw/OsSanXiit1IbdcB_sBB53gIasjwTe1dqQCLcB/s1600/rokok.gif" alt="emoticon Maaf" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*waw/g, ' <img src="https://2.bp.blogspot.com/-fMUO9AYaqgY/V2vXAwUphLI/AAAAAAAACXg/NwZ-Tlm7U7wYjbjJWYleHhnOotWC7ilOgCLcB/s1600/waw.gif" alt="emoticon mulai" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*ngopi/g, ' <img src="http://twemoji.maxcdn.com/36x36/2615.png" alt="emoticon ngopi" style="border:0; margin:0; padding:0;" id="new" /> ');
+        d[i].innerHTML = d[i].innerHTML.replace(/\s:-*hp/g, ' <img src="http://twemoji.maxcdn.com/36x36/1f4f1.png" alt="emoticon hp" style="border:0; margin:0; padding:0;" id="new" /> ');
+    }
+
+}
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(addSmiley);
+//]]>
